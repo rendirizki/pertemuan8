19f5a073e513f5a6d4bdd6aeaaa70a33d03787ec
unknown <timzone10@gmail.com>
2024-05-30 15:01:07 +0700

    p8

---
+++ pertemuan8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+<Hallo selamat datang/>
+<img src="https://www.dolanblora.com/wp-content/uploads/2024/04/evoslot168.jpg" alt="">
+</main>
+</template>
